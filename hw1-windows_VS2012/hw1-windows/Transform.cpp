35c3e5dc14269af8d981e816e911d8d1e38527a3
Jose Carlos Gallegos Romero <jcrockyeis@gmail.com>
2016-09-02 16:32:48 -0700

    Up function completed

--- hw1-windows_VS2012/hw1-windows/Transform.cpp
+++ hw1-windows_VS2012/hw1-windows/Transform.cpp
@@ -9,34 +9,51 @@
 mat3 Transform::rotate(const float degrees, const vec3& axis)
 {
   // YOUR CODE FOR HW1 HERE
-	printf("Wolo");
+	float rad = (degrees * (pi / 180.0));
+	mat3 identity = mat3(1, 0, 0, 0, 1, 0, 0, 0, 1);  
+
+	//we need to obtain matrix A 
+	mat3 A = mat3(0,axis.z, -axis.y , -axis.z , 0 , axis.x , axis.y , -axis.x , 0);
+	mat3  a = mat3((axis.x * axis.x), (axis.x * axis.y), (axis.x * axis.z), (axis.x * axis.y), (axis.y * axis.y), (axis.y * axis.z), (axis.x * axis.z), (axis.y * axis.z), (axis.z * axis.z));
+	//rodriguez formula is I3x3* cos(theta) + aaT(1 - cos (theta)) + A* sin(theta)
+	mat3 result = (identity * cos(rad)) + (a * (1 - cos(rad)))  + (A * (sin(rad)));
   // You will change this return call
-  return mat3();
+  return result;
 }
 
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
   // YOUR CODE FOR HW1 HERE
-	//Get rotation matrix
-	float rad = (degrees * (pi / 180.0f));
-	mat3 rotMatrixX = mat3(1, 0, 0, 0, cos(rad), -sin(rad), 0, sin(rad), cos(rad));
-	rotMatrixX = glm::transpose(rotMatrixX);
-	eye = eye * rotMatrixX;
-//	up = up * -(rotMatrixX);
+	printf("Eye's Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
+	printf("UP's Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", up.x, up.y, up.z, sqrt(pow(up.x, 2) + pow(up.y, 2) + pow(up.z, 2)));
+	eye = eye * rotate(degrees, up);
 
-	printf("Wolo");
+	//mat3 rotMatrixY = mat3(cos(rad), 0, sin(rad), 0, 1, 0, -sin(rad), 0, cos(rad));
+	//rotMatrixY = glm::transpose(rotMatrixY);
+	//eye = eye * rotMatrixY;
+
 }
 
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
   // YOUR CODE FOR HW1 HERE 
+	printf("Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
+	printf("UP's Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", up.x, up.y, up.z, sqrt(pow(up.x, 2) + pow(up.y, 2) + pow(up.z, 2)));
 	//Get rotation matrix 
-	float rad = (degrees * (pi / 180.0));
-	mat3 rotMatrixY = mat3(cos(rad), 0, sin(rad), 0, 1,0, -sin(rad), 0, cos(rad));
-	rotMatrixY = glm::transpose(rotMatrixY);
+	//vec3 b = up;
+	//vec3 a = eye;
+	//vec3 w = glm::normalize(a);
+	//vec3 helperU = glm::cross(b, w);
+	//vec3 u = glm::normalize(helperU);
+	//vec3 v = glm::cross(w, u);
+	//mat3 R = mat3(u.x, v.x, w.x, u.y, v.y, w.y, u.z, v.z, w.z);
+	//up = up * R;
+	//eye = eye *rotate(degrees,up);
 
+	vec3 helper = glm::normalize(glm::cross(up, eye));
+	eye = eye * rotate(degrees, helper);
+	up = up * rotate(degrees, helper);
 
-	printf("Wolo");
 
 }
 
